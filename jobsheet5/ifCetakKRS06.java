42c3deba616a97988e0db8f5229b663fcdfed5c5
aylafada <aylafada7.gmail.com>
2025-10-06 21:52:18 +0700

    Percobaan 1

--- jobsheet5/ifCetakKRS06.java
+++ jobsheet5/ifCetakKRS06.java
@@ -6,7 +6,7 @@ public class ifCetakKRS06 {
     public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
 
-    System.out.println("--- Cetak KRS SIAKAD ---");                                     
+    System.out.println("--- Cetak KRS SIAKAD ---");                             
     System.out.print("Apakah UKT sudah lunas? (true/false): ");
     boolean uktLunas = sc.nextBoolean();
 
