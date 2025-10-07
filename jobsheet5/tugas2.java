00178baa9e5b018e73cd58d706eeab02f4b79719
aylafada <aylafada7.gmail.com>
2025-10-07 16:05:19 +0700

    Tugas 2

---
+++ jobsheet5/tugas2.java
@@ -0,0 +1,19 @@
+package jobsheet5;
+
+import java.util.Scanner;
+
+public class tugas2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        //deklarasi
+        int angka;
+        System.out.println("Masukkan angka: ");
+
+        if (angka % 2 == 0) {
+            System.out.println("Angka Ganjil");
+        } else {
+            System.out.println("Angka Genap");
+        }
+    }
+}
